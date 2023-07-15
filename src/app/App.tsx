cb0b2fa1073b59a27faf01528dd562912577e39a
cogicgoran <cogicgoran99@gmail.com>
2023-07-16 00:01:42 +0200

    Init commit

---
+++ src/app/App.tsx
@@ -0,0 +1,17 @@
+import styles from './App.module.css'
+
+function App() {
+  return (
+    <main className={styles.app}>
+    <h1>Cogic Goran</h1>
+    <h2>Software Developer</h2>
+    <nav>
+      <a href="/about">About</a>
+      <a href="/career">Career</a>
+      <a href="/technology">Technologies</a>
+    </nav>
+    </main>
+  )
+}
+
+export default App
