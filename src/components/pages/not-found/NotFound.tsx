1c680b5fe479b639d80719122b63e6028274639f
cogicgoran <cogicgoran99@gmail.com>
2023-07-16 14:45:11 +0200

    Add animations to 404 page

--- src/components/pages/not-found/NotFound.tsx
+++ src/components/pages/not-found/NotFound.tsx
@@ -3,7 +3,12 @@ import ReturnHome from "../../UI/return-home/ReturnHome";
 import styles from './notFound.module.css';
 
 function NotFound() {
-    return <main className={styles.notFound}>
+    return <main className={styles.notFound}>             
+        <h1>
+            <span className={styles.digit}>4</span>
+            <span className={styles.digit}>0</span>
+            <span className={styles.digit}>4</span>
+            </h1>
         <p>Don't know what you are looking for, but it ain't here. Go <Link to='/'>home</Link></p>
         <ReturnHome />
     </main>
